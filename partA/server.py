3517eb292c40cae863789df232902951646cbaa9
Yueyang Zhang <zhan2212@cs.uchicago.edu>
2020-04-25 13:50:02 -0500

    11

--- partA/server.py
+++ partA/server.py
@@ -20,7 +20,7 @@ def annotate():
     # set up the redirect URL
     conditions = [
         {"acl": "private"},
-        ["starts-with", "$success_action_redirect", "http://zhan2212-hw3.ucmpcs.org:5000/"]
+        ["starts-with", "$success_action_redirect", "http://zhan2212-hw3.ucmpcs.org:5000/annotate/files"]
     ]
     # generate id
     UUID = str(uuid.uuid4()) # generate uuid
